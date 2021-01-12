e364a1a1b5405021f7033fd1a1300f6cd80e1053
Igna-M <ign.manzo@gmail.com>
2021-01-12 11:43:09 -0300

    Ejercicio de sitio web

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(3130, ()=>{
+    console.log('Servidor funcionando en http://localhost:3130/');
+});
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
